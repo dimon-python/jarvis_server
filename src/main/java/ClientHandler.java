d2736bf974480ae4376ef455a09a1399f0485516
dima <dimon.mazurav@gmail.com>
2025-06-25 18:46:22 +0300

    test

---
+++ src/main/java/ClientHandler.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler extends Thread{
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String message;
+    //private Boolean running = true;
+
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    public void run(){
+        try{
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            Main.setClients(this);
+
+            message = in.readLine();
+            System.out.println(message);
+
+        } catch(IOException e){
+
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.*;
+
+public class Main {
+    private static final int PORT = 12345;
+    private static Set<ClientHandler> clients = Collections.synchronizedSet(new HashSet<>());
+
+    public static void setClients(ClientHandler client){
+        clients.add(client);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Сервер запущен. Ожидание подключений...");
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                new ClientHandler(serverSocket.accept()).start();
+            }
+        }
+    }
+}
